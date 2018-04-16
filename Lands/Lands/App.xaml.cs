fb9a4a819ff5417020b175a45d91f472191b84da
ctn24ctn <ctn24ctn@users.noreply.github.com>
2018-04-16 11:54:41 -0500

    Page Lgin

--- Lands/Lands/App.xaml.cs
+++ Lands/Lands/App.xaml.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
-
+﻿
 namespace Lands
 {
-	public partial class App : Application
+    using Lands.Views;
+    using Xamarin.Forms;
+    public partial class App : Application
 	{
-		public App ()
-		{
-			InitializeComponent();
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new Lands.MainPage();
-		}
+            MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
 }
