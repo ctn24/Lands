3d4c1c75434cde46c0a696e13dc25641e9a26a83
ctn24ctn <ctn24ctn@users.noreply.github.com>
2018-04-24 10:35:12 -0500

    Molto

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,121 @@
+﻿namespace Lands.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Lands.Services;
+    using Lands.Views;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+   public class LoginViewModel : BaseViewModel
+    {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+        #region Atributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        private string email;
+
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            this.IsEnabled = true;
+
+            this.Email = "ctn24ctn@hotmail.com";
+            this.Password = "1234";
+            // http://restcountries.eu/rest/v2/all
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email.",
+                    "Accept"
+                    );
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password.",
+                    "Accept"
+                    );
+                return;
+            }
+
+            this.IsEnabled = false;
+            this.IsRunning = true;
+
+            if (this.Email != "ctn24ctn@hotmail.com" || this.Password != "1234")
+            {
+                this.IsEnabled = true;
+                this.IsRunning = false;
+                await Application.Current.MainPage.DisplayAlert(
+                        "Error",
+                        "Email or password incorrect.",
+                        "Accept"
+                        );
+                this.Password = string.Empty;
+
+                return;
+            }
+            this.IsEnabled = true;
+            this.IsRunning = false;
+
+            this.Email = string.Empty;
+            this.Password = string.Empty;
+
+            MainViewModel.GetInstance().Lands = new LandsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new LandsPage());
+        }
+        #endregion
+    }
+}
